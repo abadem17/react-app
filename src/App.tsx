d3da0e212c0246463d03636c4b8bd99d30880674
abadem17 <abad_zer0@hotmail.com>
2025-05-21 16:00:49 -0700

    project created

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import Title from "./Title";
+
+function App() {
+  return <Title />;
+}
+export default App;
---
+++ src/Title.tsx
@@ -0,0 +1,8 @@
+function Title() {
+  const name = "Abadzer0";
+  if (name) {
+    return <h1>Hola {name}</h1>;
+  }
+  return <h1>Hola Mundo</h1>;
+}
+export default Title;
